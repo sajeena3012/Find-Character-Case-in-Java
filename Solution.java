670bad1a183b11eb03eca400f0472fa85f02b73d
Rukkiah Sajeena M <144934352+sajeena3012@users.noreply.github.com>
2024-06-19 20:31:33 +0530

    Solution.java

---
+++ Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+ 
+
+class Solution {
+
+    
+
+    public static void main(String args[]) {
+
+       Scanner sc = new Scanner(System.in);
+
+       char c = sc.next().charAt(0);
+
+         if(c>='A'&&c<='Z'){
+
+           System.out.println("1");
+
+        }
+
+        else if (c>='a'&&c<='z'){
+
+           System.out.println("0");
+
+        }
+
+        else{
+
+           System.out.println("-1");
+
+        }
+
+        
+
+    }
+
+}
